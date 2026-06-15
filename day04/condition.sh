5d31c7f1834e52ff90be55ff8cf1f8f345e8eb4e
adi-dev-6354 <adityamishra6354@gmail.com>
2026-06-15 11:42:27 +0000

    Day 5 first commit.Test some shell and append using cat.

--- day04/condition.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Goood moring, continue the condition statement...  "
-num=18
-if [ $num -ge 27 ]; then 
-    echo "TEst1"
-else
-    echo "Test."
-fi
---
+++ day05/test1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter your name: " name
+
+echo "Hi $name, Papa aditya se milna nhi hai kya!"
+
+
